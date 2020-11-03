d75367be0b5bc409c3c44ebf3c4dcadb528ab714
PanovaME <ri5@mail.ru>
2020-11-03 18:53:29 +0700

    ДЗ№1 update

--- Lesson1/task1.py
+++ Lesson1/task1.py
@@ -1,3 +1,5 @@
+# Ссылка на диаграммы https://drive.google.com/file/d/1B1yg91YDLf3EcZxZNVXd3Yk4TEjLpmQ7/view?usp=sharing
+
 p = int(input("Введите целое трехзначное число: "))
 a = p // 100
 b = (p - a * 100) // 10
